dd2ac44d84c4ee65300aee88d38fe10dd0538d93
hsisco <hysensisco@gmail.com>
2020-02-23 19:17:39 -0800

    Update App.js
    PrivateRoute to BubblePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import PrivateRoute from './utils/PrivateRoute';
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <header>
+          <h1>Welcome to the Bubble App!</h1>
+        </header>
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubblepage" component={BubblePage} />
       </div>
     </Router>
   );
